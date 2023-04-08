5eccc88c48f365e975c2a91ac3bc167206000855
Jonathan Ang <j.ang2@yahoo.com>
2023-04-08 10:43:15 -0700

    completed phase 2 of error handlings

--- backend/app.js
+++ backend/app.js
@@ -7,6 +7,7 @@ const helmet = require('helmet');
 const cookieParser = require('cookie-parser');const { environment } = require('./config');
 const isProduction = environment === 'production';
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 const app = express();
 
@@ -54,8 +55,40 @@ if (!isProduction) {
 
 
 
+// Resrouces not found error handler
+  app.use((_req, _res, next) => {
+    const err = new Error("The requested resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found." };
+    err.status = 404;
+    next(err);
+  });
 
+//Sequelize error handler
+  app.use((err, _req, _res, next) => {
+    // check if error is a Sequelize error:
+    if (err instanceof ValidationError) {
+      let errors = {};
+      for (let error of err.errors) {
+        errors[error.path] = error.message;
+      }
+      err.title = 'Validation error';
+      err.errors = errors;
+    }
+    next(err);
+  });
 
+//Formatting error handler
+  app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+      title: err.title || 'Server Error',
+      message: err.message,
+      errors: err.errors,
+      stack: isProduction ? null : err.stack
+    });
+  });
 
   /******************************************************************************************************************/
   //dont edit under this line
